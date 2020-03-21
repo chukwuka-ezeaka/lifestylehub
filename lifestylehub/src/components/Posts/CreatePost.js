f0c9ff322c82bf741a1cc7531521b2b3e9d25554
wrightblessed <wrightblessed@gmail.com>
2020-03-21 17:20:52 +0100

    fixing conflict

--- lifestylehub/src/components/Posts/CreatePost.js
+++ lifestylehub/src/components/Posts/CreatePost.js
@@ -8,47 +8,36 @@ import {
   FormGroup,
   FormInput,
   FormTextarea,
-  Button,
+  Button
 } from "shards-react";
-<<<<<<< HEAD:lifestylehub/src/components/Posts/CreatePost.js
-<<<<<<< HEAD:lifestylehub/src/components/Posts/CreatePost.js
-<<<<<<< HEAD:lifestylehub/src/components/Posts/CreatePost.js
-import HttpService from "../../API";
-=======
+
 import HttpService from "../../utils/API";
->>>>>>> parent of 1afa491... changed to routing to hasrouter:src/components/Posts/CreatePost.js
-=======
-import HttpService from "../../utils/API";
->>>>>>> parent of 1afa491... changed to routing to hasrouter:src/components/Posts/CreatePost.js
-=======
-import HttpService from "../../utils/API";
->>>>>>> parent of 1afa491... changed to routing to hasrouter:src/components/Posts/CreatePost.js
+
 import LoaderSmall from "../Loaders/LoaderSmall";
 
 const _http = new HttpService();
 
-class CreatePost extends React.Component{
-    constructor(){
-        super();
-        this.state ={
-            content: '',
-            requestPending: false,
-            errMessage: '',
-            status: '',
-            tags: ''
-        }
-    }
-    
+class CreatePost extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      content: "",
+      requestPending: false,
+      errMessage: "",
+      status: "",
+      tags: ""
+    };
+  }
 
-    handleContent = (event) => {
-        this.setState({content: event.target.value});
-    }
+  handleContent = event => {
+    this.setState({ content: event.target.value });
+  };
 
-    handleTags = (event) => {
-        this.setState({tags: event.target.value});
-    }
+  handleTags = event => {
+    this.setState({ tags: event.target.value });
+  };
 
-   /* checkMimeType=(event)=>{
+  /* checkMimeType=(event)=>{
         //getting file object
         let file = event.target.file[0] 
         //define message container
@@ -69,69 +58,78 @@ class CreatePost extends React.Component{
       
       }*/
 
-    render(){
-        const { title } = this.props;
-        return (
-            <Card small className="h-100">
-                {/* Card Header */}
-                <CardHeader className="border-bottom">
-                <h6 className="m-0">{title}</h6>
-                </CardHeader>
+  render() {
+    const { title } = this.props;
+    return (
+      <Card small className="h-100">
+        {/* Card Header */}
+        <CardHeader className="border-bottom">
+          <h6 className="m-0">{title}</h6>
+        </CardHeader>
 
-                <CardBody className="d-flex flex-column">
-                
-                <Form onSubmit={this.handlePublish}>
-                   
-                    <FormGroup>
-                        <FormTextarea placeholder="Content" rows="7" onChange={this.handleContent} required/>
-                    </FormGroup>
+        <CardBody className="d-flex flex-column">
+          <Form onSubmit={this.handlePublish}>
+            <FormGroup>
+              <FormTextarea
+                placeholder="Content"
+                rows="7"
+                onChange={this.handleContent}
+                required
+              />
+            </FormGroup>
 
-                    <FormGroup>
-                        <label htmlFor="Tags">Tags</label>
-                        <FormInput placeholder="Tags" onChange={this.handleTags} required/>
-                    </FormGroup>
+            <FormGroup>
+              <label htmlFor="Tags">Tags</label>
+              <FormInput
+                placeholder="Tags"
+                onChange={this.handleTags}
+                required
+              />
+            </FormGroup>
 
-                   <FormGroup className="mb-0">
-                    <Button theme="accent" type="submit" disabled={this.state.requestPending}>
-                    {this.state.requestPending  ? <LoaderSmall/>: 'Publish'}
-                    </Button>
-                    </FormGroup>
-                </Form>
-                </CardBody>
-            </Card>
-            );
+            <FormGroup className="mb-0">
+              <Button
+                theme="accent"
+                type="submit"
+                disabled={this.state.requestPending}
+              >
+                {this.state.requestPending ? <LoaderSmall /> : "Publish"}
+              </Button>
+            </FormGroup>
+          </Form>
+        </CardBody>
+      </Card>
+    );
+  }
+
+  handlePublish = event => {
+    event.preventDefault();
+    this.setState({ requestPending: true });
+
+    const payload = {
+      content: this.state.content,
+      tags: this.state.tags,
+      status: "publish"
+    };
+    const url = "post/create";
+    _http.sendPost(url, payload).then(response => {
+      this.setState({ requestPending: false });
+      if (response.data) {
+        let type = "";
+        if (response.status === "success") {
+          type = "success";
+          _http.notify(response.message, type);
+        } else {
+          type = "warn";
+          _http.notify(response.message, type);
         }
-
-    handlePublish = (event) => {
-        event.preventDefault();
-        this.setState({requestPending: true});
-
-        const payload = {
-            "content": this.state.content,
-            "tags": this.state.tags,
-	        "status": "publish"
-        }
-        const url = 'post/create';
-        _http.sendPost(url, payload)
-        .then(response => {
-            this.setState({ requestPending: false });
-            if(response.data ){
-                let type = "";
-                if(response.status === "success"){
-                    type = "success";
-                    _http.notify(response.message, type)
-                }else{
-                    type = "warn";
-                    _http.notify(response.message, type)
-                }
-            
-            }else{
-                _http.notify(response.message)
-                this.setState({requestPending: false })
-            }
-        });
-        }
-    }
+      } else {
+        _http.notify(response.message);
+        this.setState({ requestPending: false });
+      }
+    });
+  };
+}
 
 CreatePost.propTypes = {
   /**
--- lifestylehub/src/components/applications/ChatBox.js
+++ lifestylehub/src/components/applications/ChatBox.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD:lifestylehub/src/components/applications/ChatBox.js
-import React, { Component } from "react";
-import { Card, CardBody, CardHeader, CardSubtitle } from "shards-react";
-import {
-  FormInput,
-  InputGroup,
-  Button,
-  FormGroup,
-  Col,
-  InputGroupAddon
-} from "shards-react";
-=======
 // import React, { Component } from "react";
 // import { Card, CardBody, CardTitle, CardSubtitle } from "shards-react";
 // import {
@@ -20,7 +8,6 @@ import {
 //   Col,
 //   InputGroupAddon
 // } from "shards-react";
->>>>>>> parent of 1afa491... changed to routing to hasrouter:src/components/applications/ChatBox.js
 
 // // import Jumbotron from "react-bootstrap/lib/Jumbotron";
 // // import Glyphicon from "react-bootstrap/lib/Glyphicon";
@@ -68,79 +55,6 @@ import {
 //     }
 //   }
 
-<<<<<<< HEAD:lifestylehub/src/components/applications/ChatBox.js
-  render() {
-    return (
-      <div>
-        {this.props.targetUser ? (
-          <div>
-            <NavbarComponent
-              left={
-                <div>
-                  <Col xshidden="true" smhidden="true">
-                    <p className="navBarText">
-                      {
-                        //   <Glyphicon
-                        //   onClick={this.props.onBackPressed}
-                        //   glyph="chevron-left"
-                        // />
-                      }
-                    </p>
-                  </Col>
-                  <Avatar alt={"logo"} size="large" type="circle flexible" />
-                  <p className="navBarText">{this.props.targetUser.name}</p>
-                </div>
-              }
-            />
-            <MessageList
-              className="message-list"
-              lockable={true}
-              toBottomHeight={"100%"}
-              dataSource={this.props.targetUser.messages}
-            />
-            <FormGroup>
-              <InputGroup>
-                <FormInput
-                  type="text"
-                  value={this.state.messageText}
-                  onChange={this.onMessageInputChange.bind(this)}
-                  onKeyPress={this.onMessageKeyPress.bind(this)}
-                  placeholder="Type a message here (Limit 300 characters)..."
-                  ref="messageTextBox"
-                  className="messageTextBox"
-                  maxLength="300"
-                  autoFocus
-                />
-                <InputGroupAddon type="append">
-                  <Button
-                    disabled={!this.state.messageText}
-                    className="sendButton"
-                    theme="secondary"
-                    onClick={this.onSendClicked.bind(this)}
-                  >
-                    Send
-                  </Button>
-                </InputGroupAddon>
-              </InputGroup>
-            </FormGroup>
-          </div>
-        ) : (
-          <Card className="text-center">
-            <CardHeader>
-              <h1>Hello, {(this.props.signedInUser || {}).fullname}!</h1>
-            </CardHeader>
-            <CardBody>
-              <CardSubtitle>
-                <p>Select a subscriber to start a chat.</p>
-              </CardSubtitle>
-            </CardBody>
-          </Card>
-        )}
-      </div>
-    );
-  }
-}
-=======
 //   render() {
 //     return (
 //       <div>
@@ -212,4 +126,3 @@ import {
 //     );
 //   }
 // }
->>>>>>> parent of 1afa491... changed to routing to hasrouter:src/components/applications/ChatBox.js
